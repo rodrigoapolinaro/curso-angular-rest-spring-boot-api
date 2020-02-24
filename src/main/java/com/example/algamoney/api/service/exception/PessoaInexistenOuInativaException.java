5846968af689eddc5b151ab5c9369eab62cc5b8b
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2020-02-23 23:43:05 -0300

    Deploy da API na nuvem

---
+++ src/main/java/com/example/algamoney/api/service/exception/PessoaInexistenOuInativaException.java
@@ -0,0 +1,7 @@
+package com.example.algamoney.api.service.exception;
+
+public class PessoaInexistenOuInativaException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+}
